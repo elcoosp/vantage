cb7fd557aa5891d4ce34257bf23a8018367a525d
elcoosp <elcoosp@gmail.com>
2026-07-27 16:22:46 +0200

    test(openapi): add failing test for generated OpenAPI models and interfaces

---
+++ backend/src/test/java/com/vantage/OpenApiGenerationTest.java
@@ -0,0 +1,28 @@
+package com.vantage;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class OpenApiGenerationTest {
+
+    @Test
+    void should_load_generated_product_response_model() {
+        assertDoesNotThrow(() -> {
+            Class.forName("com.vantage.api.model.ProductResponse");
+        }, "Generated ProductResponse class should be present");
+    }
+
+    @Test
+    void should_load_generated_order_request_model() {
+        assertDoesNotThrow(() -> {
+            Class.forName("com.vantage.api.model.OrderRequest");
+        }, "Generated OrderRequest class should be present");
+    }
+
+    @Test
+    void should_load_generated_product_api_interface() {
+        assertDoesNotThrow(() -> {
+            Class.forName("com.vantage.api.api.ProductApi");
+        }, "Generated ProductApi interface should be present");
+    }
+}
