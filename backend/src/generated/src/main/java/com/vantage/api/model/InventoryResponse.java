b5348f61d74c7ac77822d019d8c156db281c37d0
elcoosp <elcoosp@gmail.com>
2026-09-10 20:57:08 +0200

    feat(api): regenerate OpenAPI models and client

--- backend/src/generated/src/main/java/com/vantage/api/model/InventoryResponse.java
+++ backend/src/generated/src/main/java/com/vantage/api/model/InventoryResponse.java
@@ -20,7 +20,7 @@ import jakarta.annotation.Generated;
  * InventoryResponse
  */
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-07-27T19:18:01.480256+02:00[Europe/Paris]", comments = "Generator version: 7.5.0")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-09-10T19:54:29.637824+02:00[Europe/Paris]", comments = "Generator version: 7.5.0")
 public class InventoryResponse implements Serializable {
 
   private static final long serialVersionUID = 1L;
